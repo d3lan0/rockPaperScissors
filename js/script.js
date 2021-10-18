3b855a418be37dcac59ba84a9fa1c140c5ea199f
Theo.Higgins <theo.higgins1@gmail.com>
2021-10-17 22:07:46 -0400

    add playerRound to script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 function computerPlay(){
     let play = Math.floor(Math.random()*3)+1;
+
     if (play == 1){
         play = 'rock';
     }else if (play == 2){
@@ -7,5 +8,21 @@ function computerPlay(){
     }else play = 'scissors';
 
     return play;
+}
+
+function playRound(playerSelection, computerSelection){
+    let win = `You win, ${playerSelection} beats ${computerSelection}`;
+    let lose = `You lose, ${computerSelection} beats ${playerSelection}`;
+    let draw = `It's a draw, you both played ${computerSelection}`;
+
+    if (computerSelection === 'rock' && playerSelection === 'scissors'){
+        return lose;
+    } else if (computerSelection === 'paper' && playerSelection === 'rock'){
+        return lose;
+    } else if (computerSelection === 'scissors' && playerSelection === 'paper'){
+        return lose;
+    }else if (computerSelection === playerSelection){
+        return draw;
+    }else return win;
 }
 
