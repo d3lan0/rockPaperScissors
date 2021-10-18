90a0e6baca2629c47f46ac61f16c54d773e9a291
Theo.Higgins <theo.higgins1@gmail.com>
2021-10-17 23:12:54 -0400

    add game and checkwinner to scitp.js

--- js/script.js
+++ js/script.js
@@ -11,9 +11,9 @@ function computerPlay(){
 }
 
 function playRound(playerSelection, computerSelection){
-    let win = `You win, ${playerSelection} beats ${computerSelection}`;
-    let lose = `You lose, ${computerSelection} beats ${playerSelection}`;
-    let draw = `It's a draw, you both played ${computerSelection}`;
+    let win = [`win`,`You win, ${playerSelection} beats ${computerSelection}`];
+    let lose = ['lose',`You lose, ${computerSelection} beats ${playerSelection}`];
+    let draw = ['draw', `It's a draw, you both played ${computerSelection}`];
 
     if (computerSelection === 'rock' && playerSelection === 'scissors'){
         return lose;
@@ -26,3 +26,48 @@ function playRound(playerSelection, computerSelection){
     }else return win;
 }
 
+function checkWinner (computerScore, playerScore){
+    if (computerScore > playerScore){
+        return `Computer wins!`;
+    }else if(playerScore >computerScore){
+        return `Player wins!`;
+    } else return `Its a Draw!`;
+}
+
+function game (){
+    let computerScore = 0;
+    let playerScore = 0;
+
+     for (let i = 1; i <= 5; i++){
+        let validPlay = false;
+        let playerSelection;
+        const computerSelection = computerPlay();
+
+        do {
+            
+            playerSelection = prompt(`Please select rock, paper or scissors`).toLowerCase().toString();
+            if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors"){
+                validPlay = true;
+            }
+        }
+
+        while(!validPlay);
+
+        let round = (playRound(playerSelection,computerSelection));
+        
+        if (round[0] === 'win'){
+            console.log(round[1]);
+            playerScore++;
+        } else if(round[0] === 'lose'){
+            console.log(round[1]);
+            computerScore++
+        } else {
+            console.log(round[1]);
+        }
+    }
+    console.log(computerScore);
+    console.log(playerScore);
+    console.log(checkWinner(computerScore, playerScore));
+}
+
+game();
